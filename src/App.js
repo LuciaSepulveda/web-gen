9930bb7d9310f14416fe6c01cf64c1f221c88d99
Rocio <rocio.dithurbide@gmail.com>
2022-08-23 17:48:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Camera from './js/components/Camera/camera';
+import Button from './js/components/Button/button';
+import Cat from './js/components/Models/Cat';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Button />
+      <Canvas>
+        <Camera />
+        <Suspense fallback={null}>
+          <Cat />
+        </Suspense>
+        {/* <mesh position={[3, 0, 0]}>
+          <boxBufferGeometry args={[1.2, 1.2, 1.2]} />
+          <meshNormalMaterial />
+        </mesh>
+        <mesh position={[0, 0, 0]}>
+          <sphereBufferGeometry args={[0.8, 26, 16]} />
+          <meshNormalMaterial />
+        </mesh>
+        <mesh position={[-3, 0, 0]}>
+          <torusKnotBufferGeometry args={[0.8, 0.2, 100, 100]} />
+          <meshNormalMaterial />
+        </mesh> */}
+
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
---
+++ src/js/components/Camera/camera.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PerspectiveCamera } from '@react-three/drei';
+
+const Camera = () => {
+  return <PerspectiveCamera makeDefault position={[-10, 5, -5]} fov={25} />;
+};
+
+export default Camera;
